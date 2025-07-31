c9dd2636356cbd1700e835feb431fbdf6b69730d
Anton Shabat <anton.shabat@gmail.com>
2025-07-31 18:42:20 +0300

    fixup! week2 day4 daily

--- Week2/Day4/Daily/main.py
+++ Week2/Day4/Daily/main.py
@@ -5,8 +5,11 @@ words.sort()
 print(",".join(words))
 
 # 2
-
 # Write a function that takes a sentence as input and returns the longest word in the sentence. If there are multiple longest words, return the first one encountered. Characters like apostrophes, commas, and periods should be considered part of the word.
-
-
 words = input().split(",")
+longest_word = ""
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+
+print(longest_word)

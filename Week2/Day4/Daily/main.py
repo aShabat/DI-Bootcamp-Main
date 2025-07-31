dc1282194032101f52cf58f495bbc94ef765d9e4
Anton Shabat <anton.shabat@gmail.com>
2025-07-31 18:42:12 +0300

    week2 day4 daily

---
+++ Week2/Day4/Daily/main.py
@@ -0,0 +1,12 @@
+# 1
+# Write a Python program that takes a single string of words as input, where the words are separated by commas (e.g., ‘apple,banana,cherry’). The program should output these words sorted in alphabetical order, with the sorted words also separated by commas.
+words = input().split(",")
+words.sort()
+print(",".join(words))
+
+# 2
+
+# Write a function that takes a sentence as input and returns the longest word in the sentence. If there are multiple longest words, return the first one encountered. Characters like apostrophes, commas, and periods should be considered part of the word.
+
+
+words = input().split(",")

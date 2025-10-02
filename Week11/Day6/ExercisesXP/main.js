3d0178755fc07fceea05c98d65990e27c797932e
Anton Shabat <anton.shabat@gmail.com>
2025-10-02 17:10:01 +0300

    week11 day6 exercises

---
+++ Week11/Day6/ExercisesXP/main.js
@@ -0,0 +1,72 @@
+// 🌟 Exercise 1 : Giphy API
+// Instructions
+//
+// With your knewly accumulated knowledge of the Fetch API lets write some cool code!
+//
+//     You will work with this part of the documention
+//
+//     You will use this Gif URL: https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
+//
+// Explanation of the Gif URL and the queries
+//
+//     q Request Parameter: Search query term or phrase. Above, the URL is searching for “hilarious” gifs
+//
+//     rating Request Parameter: Filters results by specified rating. We are searching for Level 1 gifs. Check out the ratings documentation
+//
+//     api_key Request Paramater : GIPHY API Key. Our API KEY is hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
+//
+//     Create a program to retrieve the data from the API URL provided above.
+//     Use the fetch() method to make a GET request to the Giphy API and Console.log the Javascript Object that you receive.
+//     Make sure to check the status of the Response and to catch any occuring errors.
+
+const url1 =
+  "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+
+fetch(url1)
+  .then((result) => {
+    if (result.ok) {
+      return result.json();
+    } else throw `Response status is ${result.status}`;
+  })
+  .then((json) => console.log(json))
+  .catch((e) => console.log(`Error: ${e}`));
+
+// 🌟 Exercise 2 : Giphy API
+// Instructions
+//
+//     Read carefully the documention to understand all the possible queries that the URL accept.
+//     Use the Fetch API to retrieve 10 gifs about the “sun”. The starting position of the results should be 2.
+//     Make sure to check the status of the Response and to catch any occuring errors.
+//     Console.log the Javascript Object that you receive.
+
+const url2 =
+  "https://api.giphy.com/v1/gifs/search?q=sun&rating=g&limit=10&offset=2&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+
+fetch(url2)
+  .then((result) => {
+    if (result.ok) {
+      return result.json();
+    } else throw `Response status is ${result.status}`;
+  })
+  .then((json) => console.log(json))
+  .catch((e) => console.log(`Error: ${e}`));
+
+// 🌟 Exercise 3 : Async function
+// Instructions
+//
+// Improve the program below :
+
+fetch("https://www.swapi.tech/api/starships/9/")
+  .then((response) => response.json())
+  .then((objectStarWars) => console.log(objectStarWars.result));
+
+//     Create an async function, that will await for the above GET request.
+//     The program shouldn’t contain any then() method.
+//     Make sure to check the status of the Response and to catch any occuring errors.
+
+async function fetchAsync(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw `Response status is $(response.status)`;
+  const data = await response.json();
+  console.log(data.result);
+}
